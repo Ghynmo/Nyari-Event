82ef849157f769376391101b2fb06f26fbc669a6
M Ghifari <ghifarim68@gmail.com>
2021-11-26 09:01:39 +0700

    Update basic Maps UI

---
+++ src/components/MapsAPI.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+export default function MapsAPI() {
+    
+    const mapStyles = {        
+        height: "50vh",
+        width: "50%"};
+
+    const [maps, setmaps] = useState({
+        latitude: '',
+        longitude: ''
+    })
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                setmaps({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                })
+            },
+            function(error) {
+                console.error("Error Code = " + error.code + " - " + error.message);
+            }
+        );
+    }, [])
+
+    return (
+        <LoadScript
+          googleMapsApiKey='AIzaSyC6PlAlaL-9Fz4O4KwoKxGAXnnMbuKxJO4'>
+           <GoogleMap
+             mapContainerStyle={mapStyles}
+             zoom={13}
+             center={{
+                lat: maps.latitude, 
+                lng: maps.longitude
+                }}
+           />
+        </LoadScript>
+     )
+}
